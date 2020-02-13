9c6a8ba0023e2b52e0d33b5c49a5ded7b3474306
Jorge Gallegos <gallegosjorge403@gmail.com>
2020-02-12 19:53:55 -0800

    Done with challenges

--- wiki/views.py
+++ wiki/views.py
@@ -5,41 +5,28 @@ from django.views.generic.detail import DetailView
 
 
 class PageList(ListView):
-    """
-    CHALLENGES:
-      1. On GET, display a homepage that shows all Pages in your wiki.
-      2. Replace this CHALLENGE text with a descriptive docstring for PageList.
-      3. Replace pass below with the code to render a template named `list.html`.
-    """
+    """Shows a list of all wiki pages"""
     model = Page
 
     def get(self, request):
-        """ Returns a list of wiki pages. """
-        pass
+        """ Returns a list of wiki pages."""
+        wikis = Page.objects.all()
+        context = {
+          'wikis': wikis
+        }
+        return render(request, 'wiki/list.html', context)
 
 
 class PageDetailView(DetailView):
-    """
-    CHALLENGES:
-      1. On GET, render a template named `page.html`.
-      2. Replace this docstring with a description of what thos accomplishes.
-
-    STRETCH CHALLENGES:
-      1. Import the PageForm class from forms.py.
-          - This ModelForm enables editing of an existing Page object in the database.
-      2. On GET, render an edit form below the page details.
-      3. On POST, check if the data in the form is valid.
-        - If True, save the data, and redirect back to the DetailsView.
-        - If False, display all the errors in the template, above the form fields.
-      4. Instead of hard-coding the path to redirect to, use the `reverse` function to return the path.
-      5. After successfully editing a Page, use Django Messages to "flash" the user a success message
-           - Message Content: REPLACE_WITH_PAGE_TITLE has been successfully updated.
-    """
     model = Page
 
     def get(self, request, slug):
-        """ Returns a specific of wiki page by slug. """
-        pass
+        """ Returns a specific wiki page by slug. """
+        wiki = Page.objects.get(slug=slug)
+        context = {
+          'wiki': wiki
+        }
+        return render(request, 'wiki/page.html', context)
 
     def post(self, request, slug):
         pass
